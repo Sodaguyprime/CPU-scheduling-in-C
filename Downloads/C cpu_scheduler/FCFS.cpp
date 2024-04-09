8c3630f4659abac1c2bea0a1c20ae85546ef3058
Ammar Ahmed <ammarisahmed@gmail.com>
2024-04-09 05:49:36 +0000

    Added some functionality but problems with adding to the linked list

--- Downloads/C cpu_scheduler/FCFS.cpp
+++ Downloads/C cpu_scheduler/FCFS.cpp
@@ -11,24 +11,48 @@ class proccessfromfiles {
     int proccessArrivaltime;
     int processPriorty;
 };
-
-int linked_data(){
-    struct Node
+struct Node
 {
     int data;
     Node *next;
 };
 typedef Node* nodeptr;
-nodeptr head;
-head = new Node();
-head ->data = 20;
-head ->next = NULL;
 
 
+void push(nodeptr& head, int data){
+    nodeptr tempptr;
+    tempptr = new Node;
+    tempptr-> data = data;
+    tempptr-> next = NULL;
+    Node* check;
+    check = head;
+    
+    while (check->next != NULL) {
+        check = check->next;
+    }
 
-
-return head->data;
+    check->next = tempptr;
 }
+
+void Adding_to_list(nodeptr& head){
+ifstream ourinput;
+    char D;
+    ourinput.open("input.txt");
+     {
+        if(ourinput.fail()){
+            cerr<<"Error opening file"<<endl;
+        }
+    }
+    do {
+        D = ourinput.get();
+        if(D != '\n' || D !=' ' || D !=':'){
+        push(head,D);
+        }
+    }while(ourinput.good());
+    ourinput.close();
+}
+
+
 int howmanyprocess(){
     ifstream ourinput;
     char D;
@@ -50,9 +74,12 @@ int howmanyprocess(){
 }
 
 
+
 int main(){
-
-
+nodeptr head;
+head = new Node;
+head ->data = 20;
+head ->next = NULL;
     cout<<"|*---------------------------------------------------------*|"<<endl;
     cout<<"|*---------------------------------------------------------*|"<<endl;
     cout<<"|*---------------------FCFS SCHEDULING---------------------*|"<<endl;
@@ -61,8 +88,13 @@ int main(){
     cout<<"The Burst time,Arrival time and Priority for the proccess are at follows:"<<endl;
     int totalnumber = howmanyprocess();
     cout<<"The number of processes:  "<<totalnumber<<endl;
-    totalnumber = linked_data();
-    cout<<"The number of processes:  "<<totalnumber<<endl;
-
-
+    nodeptr display;
+    display = head;
+    push(head,22);
+    Adding_to_list(head);
+while (display != NULL)
+{
+    cout<<endl<<display->data;
+    display = display->next;
+}
 }
